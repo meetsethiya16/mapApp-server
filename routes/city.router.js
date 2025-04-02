fd5cc9a80fe0ee7bd028fff79394c4e5782f397a
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-04-02 15:30:05 +0530

    defined web services

--- routes/city.router.js
+++ routes/city.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 import * as cityController from "../controller/city.controller.js";
 
 router.post("/save", cityController.save);
+router.get("/cities", cityController.fetchCities);
 
 export default router;
